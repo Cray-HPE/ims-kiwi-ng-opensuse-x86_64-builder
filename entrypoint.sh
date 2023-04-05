e61288376ad6fdd25d2ee4eb0cf524c61319dfd2
David Laine <laine@hpe.com>
2023-04-05 13:59:15 -0500

    Temporarily disable arm entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,7 +55,8 @@ if [[ `echo $ENABLE_DEBUG | tr [:upper:] [:lower:]` = "true" ]]; then
 fi
 
 echo "Checking build platform: $BUILD_PLATFORM"
-if [ $BUILD_PLATFORM == "aarch64" ]; then
+#if [ $BUILD_PLATFORM == "aarch64" ]; then
+if [ 0 ]; then
     echo "Build platform is aarch64"
     # Regiser qemu-aarch64-static to act as an arm interpreter for arm builds 
     if [ ! -d /proc/sys/fs/binfmt_misc ] ; then
